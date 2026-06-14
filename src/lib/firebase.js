37fbe6e28501590ff8c56d8756b5a495eb0044de
hari <tejahari666666@gmail.com>
2026-06-14 16:49:19 +0530

    Hide Firebase secrets in environment variables

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -4,13 +4,13 @@ import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDOAczxsY0pTurgOj0q1dYd1aVhjZIHIUs",
-  authDomain: "sd-little-champs.firebaseapp.com",
-  projectId: "sd-little-champs",
-  storageBucket: "sd-little-champs.firebasestorage.app",
-  messagingSenderId: "767081158030",
-  appId: "1:767081158030:web:43c94810e77ebd0dedc0df",
-  measurementId: "G-LP06SRGHSQ"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase only once
